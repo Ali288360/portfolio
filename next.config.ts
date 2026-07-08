314b7563239ac7621668723894949fdf71d7cd4d
Abdi Ali Abdi <abdialiaa200@gmail.com>
2026-07-09 01:26:34 +0530

    fix: remove webpack config to support Next.js 16 Turbopack

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,7 @@ import path from "path";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@ui-kit': path.resolve(__dirname, 'src/ui-kits'),
-      '@type': path.resolve(__dirname, 'src/types'),
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@lib': path.resolve(__dirname, 'src/lib')
-    };
-    return config;
-  },
+
   images: {
     unoptimized: isProd ? true : false,
     remotePatterns: [
